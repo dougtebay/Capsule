06bf13dbc48661e8c0f85741ad0e71d81b6c476f
dougtebay <dougtebay@gmail.com>
2017-01-28 11:30:02 -0500

    Update User model

--- app/User.php
+++ app/User.php
@@ -2,12 +2,10 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -15,15 +13,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
+        'twitter_id', 'name', 'nickname',
     ];
 }
