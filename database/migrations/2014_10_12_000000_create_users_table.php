eb05b58610b355d4b9e6df6ba1e35589b523d35a
dougtebay <dougtebay@gmail.com>
2017-02-05 19:43:38 -0500

    Set schema string length and make twitter_id unique

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('twitter_id');
+            $table->string('twitter_id')->unique();
             $table->string('name');
             $table->string('nickname');
             $table->rememberToken();
