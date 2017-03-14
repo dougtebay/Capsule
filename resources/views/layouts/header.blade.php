16408b4f87c3efc6e1bcb5e29c0e2da27dc2deb7
dougtebay <dougtebay@gmail.com>
2017-03-14 01:42:45 -0400

    Move header elements from app blade to header blade

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,19 +2,27 @@
     <section>
         <a class="header-text" href="{{ url('/') }}">{{ config('app.name') }}</a>
     </section>
+    @if (auth()->guest())
     <section>
-        @if (auth()->guest())
-            <a href="{{ url('/login') }}">Login</a>
-        @else
-            <span class="header-text">{{ auth()->user()->name }}</span>
-            <a class="header-text"
-               href="{{ url('/logout') }}"
-               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                Logout
-            </a>
-            <form class="hidden" id="logout-form" method="POST" action="{{ url('/logout') }}">
-                {{ csrf_field() }}
-            </form>
-        @endif
+        <a class="header-text" href="{{ url('/login') }}">Login</a>
     </section>
+    @else
+    <section>
+        <form method="GET" action="/search">
+            <input type="text" name="query">
+            <button type="submit">Search</button>
+        </form>
+    </section>
+    <section>
+        <span class="header-text">{{ auth()->user()->name }}</span>
+        <a class="header-text"
+           href="{{ url('/logout') }}"
+           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            Logout
+        </a>
+        <form id="logout-form" class="hidden" method="POST" action="{{ url('/logout') }}">
+            {{ csrf_field() }}
+        </form>
+    </section>
+    @endif
 </nav>
