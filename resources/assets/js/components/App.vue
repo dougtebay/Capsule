3738a06168bb496d963bbadbb7a756c8f716e6b9
Doug Tebay <dougtebay@gmail.com>
2017-04-02 08:46:24 -0400

    Make navbar fixed

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -1,12 +1,14 @@
 <template>
-	<main>
+	<div>
 		<navbar :appName="app_name"
 			    :appUrl="app_url"
 			    :csrfToken="csrf_token"
 			    :user="user">
 		</navbar>
-		<search-results></search-results>
-	</main>
+		<main>
+			<search-results></search-results>
+		</main>
+	</div>
 </template>
 
 <script>
--- resources/assets/js/components/Navbar.vue
+++ resources/assets/js/components/Navbar.vue
@@ -1,17 +1,19 @@
 <template>
-	<nav class="navbar">
-		<section>
-	        <a class="navbar-text" href="/">{{ appName }}</a>
-	    </section>
-    	<section v-if="guest">
-        	<a class="navbar-text" href="/login">Login</a>
-    	</section>
-    	<section v-if="user">
-    		<search-form></search-form>
-	        <span class="navbar-text">{{ user.name }}</span>
-	        <a class="navbar-text" href="/logout" @click.prevent="logout">Logout</a>
-    	</section>
-	</nav>
+    <div class="navbar-container">
+    	<nav class="navbar">
+    		<section>
+    	        <a class="navbar-text" href="/">{{ appName }}</a>
+    	    </section>
+        	<section v-if="guest">
+            	<a class="navbar-text" href="/login">Login</a>
+        	</section>
+        	<section v-if="user">
+        		<search-form></search-form>
+    	        <span class="navbar-text">{{ user.name }}</span>
+    	        <a class="navbar-text" href="/logout" @click.prevent="logout">Logout</a>
+        	</section>
+    	</nav>
+    </div>
 </template>
 
 <script>
