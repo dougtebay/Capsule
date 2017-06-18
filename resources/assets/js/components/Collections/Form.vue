4c8dd83bbeb906a572d06b906220b854ade27509
Doug Tebay <dougtebay@gmail.com>
2017-06-18 19:12:16 -0400

    Add styling to cards and forms

--- resources/assets/js/components/Collections/Form.vue
+++ resources/assets/js/components/Collections/Form.vue
@@ -1,13 +1,19 @@
 <template>
-	<form>
-		<label for="title">Title</label>
-		<input id="title" type="text" v-model="collection.title">
-		<span v-if="errors.has('title')">{{ errors.get('title') }}</span>
-		<label for="description">Description</label>
-		<input id="description" type="textarea" v-model="collection.description">
-		<span v-if="errors.has('description')">{{ errors.get('description') }}</span>
-		<label for="public">Public</label>
-		<input id="public" type="checkbox" v-model="collection.public">
+	<form class="card">
+		<div>
+			<label for="title">Title</label>
+			<input class="card-input" id="title" type="text" v-model="collection.title">
+			<span v-if="errors.has('title')">{{ errors.get('title') }}</span>
+		</div>
+		<div>
+			<label for="description">Description</label>
+			<input class="card-input" id="description" type="textarea" v-model="collection.description">
+			<span v-if="errors.has('description')">{{ errors.get('description') }}</span>
+		</div>
+		<div class="card-input">
+			<label for="public">Public</label>
+			<input id="public" type="checkbox" v-model="collection.public">
+		</div>
 		<button @click.prevent="submit()">Submit</button>
 	</form>
 </template>
